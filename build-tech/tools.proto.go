e0275a408a93cfc72aa742602aa395cda16e7bcd
Rod Morison <rmorison@users.noreply.github.com>
2022-01-02 12:59:49 -0800

    Add tools.go for protobuf toolchain

---
+++ build-tech/tools.proto.go
@@ -0,0 +1,11 @@
+//go:build tools
+// +build tools
+
+// Starting scaffold for protoc tools module
+
+package tools
+
+import (
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
